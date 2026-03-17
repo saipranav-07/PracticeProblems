01089853cecf3873df9a410a596db650548ab09b
sv9274@srmist.edu.in <your.email@example.com>
2026-03-17 14:00:36 +0530

    Multiplication

---
+++ Multiplication.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int number;
+        int[] table = new int[10];
+
+        // Input
+        System.out.print("Enter a number: ");
+        number = sc.nextInt();
+
+        // Store multiplication results
+        for (int i = 1; i <= 10; i++) {
+            table[i - 1] = number * i;
+        }
+
+        // Display table
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(number + " * " + i + " = " + table[i - 1]);
+        }
+
+        sc.close();
+    }
+}
